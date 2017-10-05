ab4224698558ce61e8cc59a7db2d243c761633d5
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-10-05 16:25:35 +0200

    RenderState to Viewer record.

--- src/Scene/Viewer.hs
+++ src/Scene/Viewer.hs
@@ -10,11 +10,14 @@ module Scene.Viewer
     ) where
 
 import           Control.Concurrent.Async (Async, wait)
+import           Control.Concurrent.STM   (TVar)
+import           Scene.Types              (RenderState)
 
 -- | The viewer record is a handle from the application to the runtime of
 -- the viewer library. To the user the record is opaque.
 data Viewer = Viewer
     { renderThread :: !(Async ())
+    , renderState  :: !(TVar RenderState)
     }
 
 -- | Wait until the render thread has terminated.
